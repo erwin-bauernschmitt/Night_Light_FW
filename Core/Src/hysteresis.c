f8a4e13f51b558ba26d97f08d137af1e46db2278
erwin-bauernschmitt <110959850+erwin-bauernschmitt@users.noreply.github.com>
2023-12-28 00:10:27 +0800

    Implemented SWO for auto on/off and reformatted
    - SWO messages with on/off transition, threshold values and lux reading are now transmitted.
    - Reformatted the code to conform with the K&R format.

--- Core/Src/hysteresis.c
+++ Core/Src/hysteresis.c
@@ -21,30 +21,34 @@ void check_for_on_off(uint32_t *hysteresis_thresholds) {
 	if (mlux_reading < hysteresis_thresholds[0]) {
 		if (current_state == STANDBY) {
 			event_flag = AMBIENT_LIGHT_TURN_ON;
+			printf("\nTURNING ON:\n");
+			printf("Thresholds: %lu mlux, %lu mlux\n", hysteresis_thresholds[0],
+					hysteresis_thresholds[1]);
+			printf("Light reading: %lu mlux\n", mlux_reading);
 		}
-	}
-	else if (mlux_reading > hysteresis_thresholds[1]) {
+	} else if (mlux_reading > hysteresis_thresholds[1]) {
 		if ((current_state == WHITE_LIGHT) || (current_state == RGB_LIGHT)) {
 			event_flag = AMBIENT_LIGHT_TURN_OFF;
+			printf("\nTURNING OFF:\n");
+			printf("Thresholds: %lu mlux, %lu mlux\n", hysteresis_thresholds[0],
+					hysteresis_thresholds[1]);
+			printf("Light reading: %lu mlux\n", mlux_reading);
 		}
 	}
 }
 
-
 void update_hysteresis_thresholds(uint32_t *hysteresis_thresholds) {
-    /* Define scale factor for the logarithmic mapping. */
-    double scale_factor = (log(MAX_LUX) - log(MIN_LUX)) / log(ADC_RES - 1);
+	/* Define scale factor for the logarithmic mapping. */
+	double scale_factor = (log(MAX_LUX) - log(MIN_LUX)) / log(ADC_RES - 1);
 
-    /* Apply logarithmic mapping. */
-    double threshold = exp(log(MIN_LUX) + scale_factor * log(pot3_moving_average));
+	/* Apply logarithmic mapping. */
+	double threshold = exp(
+			log(MIN_LUX) + scale_factor * log(pot3_moving_average));
 
-    /* Determine hysteresis factor. */
-    double hysteresis_factor = 0.2;
+	/* Determine hysteresis factor. */
+	double hysteresis_factor = 0.2;
 
-    /* Set the hysteresis thresholds. */
-    hysteresis_thresholds[0] = (uint32_t)threshold;
-    hysteresis_thresholds[1] = (uint32_t)(threshold * (1 + hysteresis_factor));
-
-    printf("thresholds: %lu, %lu\n", hysteresis_thresholds[0], hysteresis_thresholds[1]);
-    printf("mlux reading: %lu\n", mlux_reading);
+	/* Set the hysteresis thresholds. */
+	hysteresis_thresholds[0] = (uint32_t) threshold;
+	hysteresis_thresholds[1] = (uint32_t) (threshold * (1 + hysteresis_factor));
 }
